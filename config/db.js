5ad4f232b1dba8fa37506d5698c55c5d79d3d516
Fer Catalano <fernando@hazak.tech>
2024-06-30 19:27:16 -0300

    Add -  env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 import sequelize from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const db = new sequelize('node_agencia', 'root', 'root', {
-    host: '127.0.0.1',
-    port: 3306,
+console.log(process.env.DB_NAME);
+
+const db = new sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
