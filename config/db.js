90da2c3ed88067dfcb8ba9aa7b74bfd1928163ab
Fer Catalano <fernando@hazak.tech>
2024-06-30 19:33:46 -0300

    Add -  start

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ console.log(process.env.DB_NAME);
 
 const db = new sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3306, 
     dialect: 'mysql',
     define: {
         timestamps: false
